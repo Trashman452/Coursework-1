52639f4eef20cec21db67d566c5ab40fdb5f146b
Trashman452 <osipovkirill452@gmail.com>
2026-04-23 19:45:46 +0700

    BudgetFormNotYet

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("article-form").classList.add("active")
+}
+
+function closeForm() {
+    document.getElementById("article-form").classList.remove("active")
+}
